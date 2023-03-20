3ef4c39b4e9b558a1e9806d91d8f767dc703cc4c
Fauxpaw <fauxpawsc@live.com>
2023-03-20 14:32:07 -0700

    final

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michael Sweeney on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
